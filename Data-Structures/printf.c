24f03b848a6d67f8f8d1a881ee5a3c213854b4a1
JRSkates <skatesproduction@gmail.com>
2025-01-08 10:07:24 +0000

    printf syntax for different data types

---
+++ Data-Structures/printf.c
@@ -0,0 +1,9 @@
+// %d or %i	   Integer (decimal)	                printf("%d", 10);	    10
+// %u	       Unsigned integer	                    printf("%u", 10);	    10
+// %f	       Floating-point (decimal notation)	printf("%f", 3.14);	    3.140000
+// %e or %E	   Scientific notation (exponent)	    printf("%e", 3.14);	    3.140000e+00
+// %g or %G	   Shorter of %f or %e format	        printf("%g", 3.14);	    3.14
+// %c	       Character	                        printf("%c", 'A');	    A
+// %s	       String	                            printf("%s", "Hello");	Hello
+// %p	       Pointer address	                    printf("%p", ptr);	    0x7ffee6
+// %%	       Prints % itself	                    printf("%%");	        %

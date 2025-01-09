09b1006e04a52641f01b1a485d187da5ce2ec6f9
JRSkates <skatesproduction@gmail.com>
2025-01-09 11:29:07 +0000

    struct exercise reformatted

--- Data-Structures/struct_values.c
+++ Data-Structures/struct_values.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-     struct Employee {
-        int id;
-        char name[50];
-        char position[50];
-        float salary;
-    };
+// Define struct outside of main
+typedef struct Employee {
+    int id;
+    char name[50];
+    char position[50];
+    float salary;
+} employee;
 
-    struct Employee e1 = {1, "John Doe", "Software Engineer", 80000.0};
-    struct Employee e2 = {2, "Jog Bloggs", "Solutions Architect", 65000.0};
+int main(void) {
+    employee e1 = {1, "John Doe", "Software Engineer", 80000.0};
+    employee e2 = {2, "Jog Bloggs", "Solutions Architect", 65000.0};
 
     printf("Employee ID %d\n", e1.id);
     printf("Employee Name: %s\n", e1.name);
@@ -21,6 +22,8 @@ int main() {
     printf("Employee Name: %s\n", e2.name);
     printf("Employee Position: %s\n", e2.position);
     printf("Employee Salary: £%.2f\n", e2.salary);
+
+    return 0;
 }
 
 // gcc -o struct_values struct_values.c

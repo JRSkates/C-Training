e9692586c5f2fccf14af7ff888c90f003ca53b9d
JRSkates <skatesproduction@gmail.com>
2025-01-09 13:04:15 +0000

    Logical operators

--- Logical-Statements/logical_statements.c
+++ Logical-Statements/logical_statements.c
@@ -18,6 +18,19 @@ int main() {
     // a <= b: 1
     printf("a >= b: %d\n", a >= b);
     // a >= b: 0
+    printf("\n");
+
+
+    // Logical Operators 
+    // Logical operators combine or negate boolean expressions
+    int c = 10, d = -5;
+
+    printf("(c > 0) && (d > 0): %d\n", (c > 0) && (d > 0));
+    // c > 0) && (d > 0): 0
+    printf("(c > 0) || (d > 0): %d\n", (c > 0) || (d > 0));
+    // (c > 0) || (d > 0): 1
+    printf("!(c > 0): %d\n", !(c > 0));
+    // !(c > 0): 0
 
     return 0;
 }

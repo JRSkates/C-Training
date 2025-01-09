9f15acb52786d0e87a90077e0fe2d785d2b2e9b8
JRSkates <skatesproduction@gmail.com>
2025-01-09 13:28:34 +0000

    swtich statements

--- Logical-Statements/logical_statements.c
+++ Logical-Statements/logical_statements.c
@@ -22,6 +22,7 @@ int main() {
 
 
 
+
     // Logical Operators 
     // Logical operators combine or negate boolean expressions
     int c = 10, d = -5;
@@ -34,6 +35,7 @@ int main() {
     // !(c > 0): 0
     printf("\n");
     
+
 
 
     // Conditional Statements
@@ -53,6 +55,42 @@ int main() {
         printf("The number is non-positive.\n");
     }
     // The number is non-positive.
+    printf("\n");
+
+
+
+
+    // Switch Statements
+    // Executes a block of code based on a condition
+    int day = 4;
+
+    switch (day) {
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        case 7:
+            printf("Sunday\n");
+            break;
+        default:
+            printf("Invalid day number.\n");
+    }
+    // Thursday
+    printf("\n");
 
     return 0;
 }

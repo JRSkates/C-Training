84746f58deeee24f48166bba13d275ab1da89d82
JRSkates <skatesproduction@gmail.com>
2025-01-09 13:21:00 +0000

    ConditionalStatements

--- Logical-Statements/logical_statements.c
+++ Logical-Statements/logical_statements.c
@@ -21,6 +21,7 @@ int main() {
     printf("\n");
 
 
+
     // Logical Operators 
     // Logical operators combine or negate boolean expressions
     int c = 10, d = -5;
@@ -31,6 +32,27 @@ int main() {
     // (c > 0) || (d > 0): 1
     printf("!(c > 0): %d\n", !(c > 0));
     // !(c > 0): 0
+    printf("\n");
+    
+
+
+    // Conditional Statements
+    // Exectues a block of code if the condition is true
+    int num = 10;
+
+    if (num > 0) {
+        printf("The number is positive.\n");
+    }
+    // The number is positive.
+
+    int num_two = -5;
+
+    if (num_two > 0) {
+        printf("The number is positive.\n");
+    } else {
+        printf("The number is non-positive.\n");
+    }
+    // The number is non-positive.
 
     return 0;
 }

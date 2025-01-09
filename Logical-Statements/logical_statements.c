5a697ceadccfd9746a5d4fb1c5f03702cb2244ed
JRSkates <skatesproduction@gmail.com>
2025-01-09 12:44:35 +0000

    Logical Operator setup, relational operators

---
+++ Logical-Statements/logical_statements.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // Relational Operators
+    // Relational operators compare two values 
+    // and return a boolean result (true or false).
+    int a = 10, b = 20;
+
+    printf("a == b: %d\n", a == b);
+    // a == b: 0
+    printf("a != b: %d\n", a != b);
+    // a != b: 1
+    printf("a < b: %d\n", a < b);
+    // a < b: 1
+    printf("a > b: %d\n", a > b);
+    // a > b: 0
+    printf("a <= b: %d\n", a <= b);
+    // a <= b: 1
+    printf("a >= b: %d\n", a >= b);
+    // a >= b: 0
+
+    return 0;
+}

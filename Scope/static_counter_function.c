826cd0f64ae3c9ae86df082ab2f5f6ba30884fb6
JRSkates <skatesproduction@gmail.com>
2025-01-10 13:57:29 +0000

    static counter function exercise solution

---
+++ Scope/static_counter_function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void counter_function();
+
+int main(void) {
+    for (int i = 0; i < 10; i++) {
+        counter_function(); // Call the function within the loop
+    }
+
+    return 0;
+}
+
+void counter_function() {
+    static int counter = 0; // Static variable to retain its value between function calls
+    counter++;
+    printf("Counter: %d\n", counter);
+}

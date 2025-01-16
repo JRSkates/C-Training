d4ae9f8ac3ad5b69436ce02a5bc2e0d0cc7a39a1
JRSkates <skatesproduction@gmail.com>
2025-01-16 16:34:41 +0000

    string exercise 2 completed

---
+++ String-Parsing/exercise2.c
@@ -0,0 +1,24 @@
+// Write a program to count the total number of words in a string.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "My String has  eight words again again again";
+    int length = strlen(str) - 1; // length excluding null terminator
+    int count = 0;
+    char prev;
+
+    for(int i = 0; i < length; i++) {
+        if(str[i] == ' ' && prev != ' ') { // 
+            count++;
+        }
+        prev = str[i];
+    }
+    // increments counter if character is a space
+    // also checks if previous character is NOT a space
+    // this allows for double spaces
+
+    printf("Number of words: %d\n", count + 1); // Add one for the last word
+    return 0;
+}

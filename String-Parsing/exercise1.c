32074c2cf4f8c22c5df4d838125d032058325d3d
JRSkates <skatesproduction@gmail.com>
2025-01-16 16:20:34 +0000

    string exercise 1 complete

---
+++ String-Parsing/exercise1.c
@@ -0,0 +1,13 @@
+// Write a program to print individual characters of a string in reverse order.
+
+#include <stdio.h>
+#include <string.h> // For string functions
+
+int main() {
+    char str[] = "My String!";
+    int length = strlen(str);
+
+    for(int i = 0; i < length; i++) {
+        printf("%c\n", str[length - 1 - i]); // Print characters in reverse order
+    }
+}
